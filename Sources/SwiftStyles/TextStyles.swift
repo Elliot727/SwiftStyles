26eb1946f20288480dd3e3abcaa6f20ff5c98b4e
Elliot727 <39097740+Elliot727@users.noreply.github.com>
2024-05-09 15:50:02 +0100

    Made all text styles public

--- Sources/SwiftStyles/TextStyles.swift
+++ Sources/SwiftStyles/TextStyles.swift
@@ -4,52 +4,52 @@ import SwiftUI
 @available(iOS 16, *)
 public class FontManager {
     // Shared instance
-    static let shared = FontManager()
+    public static let shared = FontManager()
     
     // Titles
-    let titleLarge = Font.system(.largeTitle, design: .rounded).weight(.regular)
-    let titleMedium = Font.system(.title, design: .rounded).weight(.semibold)
-    let titleSmall = Font.system(.title2, design: .rounded).weight(.regular)
-    let titleTiny = Font.system(.title3, design: .rounded).weight(.light)
-
+    public let titleLarge = Font.system(.largeTitle, design: .rounded).weight(.regular)
+    public let titleMedium = Font.system(.title, design: .rounded).weight(.semibold)
+    public let titleSmall = Font.system(.title2, design: .rounded).weight(.regular)
+    public let titleTiny = Font.system(.title3, design: .rounded).weight(.light)
+    
     // Headers
-    let headerLarge = Font.system(.headline, design: .rounded).weight(.semibold)
-    let headerMedium = Font.system(.subheadline, design: .rounded).weight(.regular)
-    let headerSmall = Font.system(.callout, design: .rounded).weight(.medium)
-    let headerTiny = Font.system(.footnote, design: .rounded).weight(.light)
-    let headerCaption = Font.system(.caption, design: .rounded).weight(.regular)
-
+    public let headerLarge = Font.system(.headline, design: .rounded).weight(.semibold)
+    public let headerMedium = Font.system(.subheadline, design: .rounded).weight(.regular)
+    public let headerSmall = Font.system(.callout, design: .rounded).weight(.medium)
+    public let headerTiny = Font.system(.footnote, design: .rounded).weight(.light)
+    public let headerCaption = Font.system(.caption, design: .rounded).weight(.regular)
+    
     // Body
-    let bodyRegular = Font.system(.body, design: .rounded).weight(.regular)
-    let bodyBold = Font.system(.body, design: .rounded).weight(.semibold)
-    let bodyLight = Font.system(.callout, design: .rounded).weight(.light)
-    let bodyFootnote = Font.system(.footnote, design: .rounded).weight(.regular)
-    let bodyCaption = Font.system(.caption, design: .rounded).weight(.medium)
-
+    public let bodyRegular = Font.system(.body, design: .rounded).weight(.regular)
+    public let bodyBold = Font.system(.body, design: .rounded).weight(.semibold)
+    public let bodyLight = Font.system(.callout, design: .rounded).weight(.light)
+    public let bodyFootnote = Font.system(.footnote, design: .rounded).weight(.regular)
+    public let bodyCaption = Font.system(.caption, design: .rounded).weight(.medium)
+    
     // Buttons
-    let buttonLarge = Font.system(.title2, design: .rounded).weight(.bold)
-    let buttonMedium = Font.system(.callout, design: .rounded).weight(.semibold)
-    let buttonSmall = Font.system(.footnote, design: .rounded).weight(.regular)
-
+    public  let buttonLarge = Font.system(.title2, design: .rounded).weight(.bold)
+    public  let buttonMedium = Font.system(.callout, design: .rounded).weight(.semibold)
+    public  let buttonSmall = Font.system(.footnote, design: .rounded).weight(.regular)
+    
     // Labels
-    let labelLight = Font.system(.title3, design: .rounded).weight(.light)
-    let labelRegular = Font.system(.callout, design: .rounded).weight(.regular)
-    let labelMedium = Font.system(.footnote, design: .rounded).weight(.medium)
-    let labelBold = Font.system(.caption, design: .rounded).weight(.semibold)
-
+    public let labelLight = Font.system(.title3, design: .rounded).weight(.light)
+    public  let labelRegular = Font.system(.callout, design: .rounded).weight(.regular)
+    public  let labelMedium = Font.system(.footnote, design: .rounded).weight(.medium)
+    public  let labelBold = Font.system(.caption, design: .rounded).weight(.semibold)
+    
     // Captions
-    let captionRegular = Font.system(.caption, design: .rounded).weight(.regular)
-    let captionLight = Font.system(.caption2, design: .rounded).weight(.light)
-
+    public  let captionRegular = Font.system(.caption, design: .rounded).weight(.regular)
+    public  let captionLight = Font.system(.caption2, design: .rounded).weight(.light)
+    
     // Monospaced
-    let monoRegular = Font.system(.body, design: .monospaced).weight(.regular)
-    let monoMedium = Font.system(.callout, design: .monospaced).weight(.medium)
-    let monoBold = Font.system(.footnote, design: .monospaced).weight(.semibold)
-    let monoLight = Font.system(.caption, design: .monospaced).weight(.light)
-
+    public let monoRegular = Font.system(.body, design: .monospaced).weight(.regular)
+    public  let monoMedium = Font.system(.callout, design: .monospaced).weight(.medium)
+    public let monoBold = Font.system(.footnote, design: .monospaced).weight(.semibold)
+    public let monoLight = Font.system(.caption, design: .monospaced).weight(.light)
+    
     // Additional Styles
-    let italicBody = Font.system(.body, design: .serif).italic()
-    let italicCallout = Font.system(.callout, design: .serif).italic()
-    let italicFootnote = Font.system(.footnote, design: .serif).italic()
-    let italicCaption = Font.system(.caption, design: .serif).italic()
+    public let italicBody = Font.system(.body, design: .serif).italic()
+    public let italicCallout = Font.system(.callout, design: .serif).italic()
+    public let italicFootnote = Font.system(.footnote, design: .serif).italic()
+    public let italicCaption = Font.system(.caption, design: .serif).italic()
 }
